52eb2f42f9a4a6543944150ec2fb08c200a02c74
SayanNag1211 <SayanNag1211@users.noreply.github.com>
2025-07-11 19:37:27 +0000

    deploy: edc951e699e7e71688faf589277e836819c2f30f

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"72f2b18bb094f92f62a3113a8075240ebb59af
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3195795928"
+    serviceWorkerVersion: "1862804498"
   }
 });
